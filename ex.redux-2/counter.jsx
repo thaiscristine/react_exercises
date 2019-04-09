e7c297116cc68f2dab2fd0be131ae2ec4c7084d4
Thais Cristine <this.cristine06@gmail.com>
2019-04-09 13:11:13 +0300

    - organizing folders - study finished

---
+++ ex.redux-2/counter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { inc, dec, stepChanged } from './counterActions'
+
+// como tem apenas um parametro, os parenteses sao opcionais em (props)
+const Counter = (props) => (
+    <div>
+        <h1>{props.counter.number}</h1>
+        <input onChange={ props.stepChanged}
+            value={props.counter.step} type='number'/>  
+        <button onClick={props.dec}>-</button>
+        <button onClick={props.inc}>+</button>
+    </div>
+)
+
+const mapStateToProps = state => ({ counter: state.counter })
+const mapDispatchToProps = 
+    dispatch => bindActionCreators({
+        inc, dec, stepChanged
+    }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
